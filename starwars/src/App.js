27cfb347946845fed4f57992cbd4546a7c7714f9
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2019-07-22 13:13:18 -0500

    WIP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -9,7 +9,6 @@ let swapiAPI = 'https://swapi.co/api/people/'
 
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -21,7 +20,6 @@ function App() {
     axios.get(API)
     .then(res => {
       setContent(res.data.results);
-
     })
     .catch(e => {
       console.log(e)
